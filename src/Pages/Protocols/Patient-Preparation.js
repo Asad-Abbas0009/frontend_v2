4e894ffcc96117ebb72f29f19f79862e5dadbfd4
Asad-Abbas <127695373+Asad-Abbas0009@users.noreply.github.com>
2025-02-09 17:46:06 +0530

    Update Patient-Preparation.js

--- src/Pages/Protocols/Patient-Preparation.js
+++ src/Pages/Protocols/Patient-Preparation.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+const PatientPreparation = ({ title, description }) => {
+  return (
+    <div>
+      <h1>{title || "Default Title"}</h1>
+      <p>{description || "This is a default description."}</p>
+    </div>
+  );
+};
+
+export default PatientPreparation;
