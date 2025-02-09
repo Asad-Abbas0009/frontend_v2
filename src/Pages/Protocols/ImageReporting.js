859849e0ea630c8c240bb858f09d3abecbbbcd4e
Asad-Abbas <127695373+Asad-Abbas0009@users.noreply.github.com>
2025-02-09 17:45:14 +0530

    Update ImageReporting.js

--- src/Pages/Protocols/ImageReporting.js
+++ src/Pages/Protocols/ImageReporting.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+const ImageReporting = ({ title, description }) => {
+  return (
+    <div>
+      <h1>{title || "Default Title"}</h1>
+      <p>{description || "This is a default description."}</p>
+    </div>
+  );
+};
+
+export default ImageReporting;
 
