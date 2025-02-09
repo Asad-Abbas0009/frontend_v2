643d090d7d0fc22f61530ab4db1aeee9b4e9c00d
Asad-Abbas <127695373+Asad-Abbas0009@users.noreply.github.com>
2025-02-09 17:44:04 +0530

    Update ImageAcquisition.js

--- src/Pages/Protocols/ImageAcquisition.js
+++ src/Pages/Protocols/ImageAcquisition.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+const ImageAcquisition = ({ title, description }) => {
+  return (
+    <div>
+      <h1>{title || "Default Title"}</h1>
+      <p>{description || "This is a default description."}</p>
+    </div>
+  );
+};
+
+export default ImageAcquisition;
