141eb8990bda1f2c2688c530a04a4a784f8b5dd1
Asad-Abbas <127695373+Asad-Abbas0009@users.noreply.github.com>
2025-02-09 17:48:25 +0530

    Update PreCounselling.js

--- src/Pages/Protocols/PreCounselling.js
+++ src/Pages/Protocols/PreCounselling.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+const PreCounselling = ({ title, description }) => {
+  return (
+    <div>
+      <h1>{title || "Default Title"}</h1>
+      <p>{description || "This is a default description."}</p>
+    </div>
+  );
+};
+
+export default PreCounselling;
