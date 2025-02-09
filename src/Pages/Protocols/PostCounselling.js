9dbd1c13719f459740f0698114137999c00446a6
Asad-Abbas <127695373+Asad-Abbas0009@users.noreply.github.com>
2025-02-09 17:47:53 +0530

    Update PostCounselling.js

--- src/Pages/Protocols/PostCounselling.js
+++ src/Pages/Protocols/PostCounselling.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+const PostCounselling = ({ title, description }) => {
+  return (
+    <div>
+      <h1>{title || "Default Title"}</h1>
+      <p>{description || "This is a default description."}</p>
+    </div>
+  );
+};
+
+export default PostCounselling;
